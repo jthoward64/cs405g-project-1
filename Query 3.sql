37a0352c825b7ff9508fcdb7209685d6808d0cf7
Mike <58347846+mklstacy@users.noreply.github.com>
2023-10-05 20:34:09 -0400

    Update Query 3
    New Output for updated Receipt_Table

--- Query 3.sql
+++ Query 3.sql
@@ -23,23 +23,23 @@ INNER JOIN Entity_Table AS Buyer ON ReceiptTable.Buyer_Key = Buyer.Entity_ID;
 
 -- Output Produced as of 7:45 PM EST.
 /*
-+----------------------+-----------------------+-----------------+-----------------+-------+-------+
-| Customer name        | Business Name         | Highest Price   | Total Sale      | State | Zip   |
-+----------------------+-----------------------+-----------------+-----------------+-------+-------+
-| Mr. Ray L. Hyatt Jr. | Kroger                |            5.00 |           25.00 | KY    | 40503 |
-| Michael Stacy        | The Home Depot        |            6.99 |           30.00 | KY    | 40504 |
-| Target               | Chick-fil-A           |           13.37 |           29.75 | KY    | 40502 |
-| Tag Howard           | Target                |           12.00 |           36.04 | KY    | 40509 |
-| John Wick            | DV8 Kitchen           |           15.00 |           30.02 | KY    | 40504 |
-| Dr. Stephen Strange  | Kroger                |            6.49 |           22.10 | KY    | 40503 |
-| Tag Howard           | Headliners Music Hall |           16.53 |           16.53 | KY    | 40508 |
-| Costco Wholesale     | Kroger                |           23.00 |           47.44 | KY    | 40503 |
-| Jersey Mike's Subs   | Culver's              |            9.99 |           25.00 | KY    | 40509 |
-| Target               | Magic Kingdom Park    |           15.99 |           78.25 | FL    | 32830 |
-| Target               | Walmart Supercenter   |           20.83 |           57.80 | KY    | 40511 |
-| Tag Howard           | Target                |           30.67 |           80.97 | KY    | 40508 |
-| Michael Stacy        | Jersey Mike's Subs    |           15.60 |           40.28 | KY    | 40527 |
-| Michael Stacy        | Busch Stadium         |        36213.35 |        36217.34 | DC    | 20500 |
-| Pentagon             | White House           | 705392000000.00 | 705392000000.00 | DC    | 20301 |
-+----------------------+-----------------------+-----------------+-----------------+-------+-------+
++-------------------------+-----------------------+-----------------+-----------------+-------+
+| Customer name           | Business Name         | Highest Price   | Total Sale      | State |
++-------------------------+-----------------------+-----------------+-----------------+-------+
+| Mr. Ray L. Hyatt Jr.    | Kroger                |            5.00 |           25.00 | KY    |
+| Michael Stacy           | The Home Depot        |            6.99 |           30.00 | KY    |
+| Tony Stark              | Chick-fil-A           |           13.37 |           29.75 | KY    |
+| Tag Howard              | Target                |           12.00 |           36.04 | KY    |
+| John Wick               | DV8 Kitchen           |           15.00 |           30.02 | KY    |
+| Dr. Stephen Strange     | Kroger                |            6.49 |           22.10 | KY    |
+| Tag Howard              | Headliners Music Hall |           16.53 |           16.53 | KY    |
+| Mr. Ray L. Hyatt Jr.    | Kroger                |           23.00 |           47.44 | KY    |
+| President Eli Capilouto | Culver's              |            9.99 |           25.00 | KY    |
+| Bob Porter c/o Intech   | Magic Kingdom Park    |           15.99 |           78.25 | FL    |
+| Bob C. Smith            | Walmart Supercenter   |           20.83 |           57.80 | KY    |
+| Tag Howard              | Target                |           30.67 |           80.97 | KY    |
+| Michael Stacy           | Jersey Mike's Subs    |           15.60 |           40.28 | KY    |
+| Michael Stacy           | Busch Stadium         |        36213.35 |        36217.34 | DC    |
+| Pentagon                | White House           | 705392000000.00 | 705392000000.00 | DC    |
++-------------------------+-----------------------+-----------------+-----------------+-------+
 */
